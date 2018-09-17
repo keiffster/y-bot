9895c8af20d64e1e5f7a2e9c3d46402788e15635
keithsterling <keith@keithsterling.com>
2018-09-17 09:46:50 +0100

    Final stages of storage engine upgrade

--- scripts/xnix/y-bot.sh
+++ scripts/xnix/y-bot.sh
@@ -2,5 +2,10 @@
 
 clear
 
+if [[ -z "${PYTHONPATH}" ]]; then
+  echod "PYTHONPATH is undefined"
+  exit
+fi
+
 python3 -m programy.clients.events.console.client --config ../../configconfig.yaml --cformat yaml --logging ../../config/logging.yaml
 
