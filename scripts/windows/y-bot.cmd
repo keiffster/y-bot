44a9d83b068c51b209aa64ab8f9c2711423bc979
keithsterling <keith@keithsterling.com>
2018-10-15 08:43:02 +0100

    Windows config file now renamed to match other bots

--- scripts/windows/y-bot.cmd
+++ scripts/windows/y-bot.cmd
@@ -5,5 +5,5 @@ CLS
 IF NOT DEFINED PYTHONPATH (
     ECHO PYTHONPATH not set
 ) ELSE (
-    python -m programy.clients.events.console.client --config ..\..\config\windows\config.console.yaml --cformat yaml --logging ..\..\config\windows\logging.yaml
+    python -m programy.clients.events.console.client --config ..\..\config\windows\config.yaml --cformat yaml --logging ..\..\config\windows\logging.yaml
 )
