5fc31917d862b4479661728c6426a045f61f9344
Keith Sterling <keiffster@gmail.com>
2019-02-13 20:39:56 +0000

    Updates to windows installation files

---
+++ scripts/windows/y-bot-flask-rest.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+python -m programy.clients.restful.flask.client --config ..\..\config\windows\config.flask.rest.yaml --cformat yaml --logging ..\..\config\windows\logging.yaml
+
---
+++ scripts/windows/y-bot-webchat.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+python -m programy.clients.restful.flask.webchat.client --config ..\..\config\windows\config.webchat.yaml --cformat yaml --logging ..\..\config\windows\logging.yaml
+
