6cc8d05f075dcf37d7de16ca0060c71b74089cab
Keith Sterling <keiffster@gmail.com>
2019-05-01 21:47:05 +0100

    Updated for 3.8 release

---
+++ scripts/xnix/y-bot-discord.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+clear
+
+
+python3 -m programy.clients.events.discord.client --config ../../config/xnix/config.discord.yaml --cformat yaml --logging ../../config/xnix/logging.yaml
+
---
+++ scripts/xnix/y-bot-microsoft.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+clear
+
+
+python3 -m programy.clients.restful.asyncio.microsoft.client --config ../../config/xnix/config.microsoft.yaml --cformat yaml --logging ../../config/xnix/logging.yaml
+
